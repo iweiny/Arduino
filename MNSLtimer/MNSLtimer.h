e5a2afdd7ffa48597c6e40b4e8b136ce77ed3318
Ira Weiny <weiny2@llnl.gov>
2014-06-15 15:46:34 -0700

    add comments

--- MNSLtimer/MNSLtimer.h
+++ MNSLtimer/MNSLtimer.h
@@ -8,13 +8,16 @@
 #define SERIAL_DEBUG 1
 #include "serial_debug.h"
 
+/* Button defines */
 #define NO_BUTTON      0
 #define START_BUTTON   1
 #define CANCEL_BUTTON  2
 
+/* Keypad defines */
 #define KEYPAD_STAR    10
 #define KEYPAD_POUND   11
 
+/* global functions */
 void sound_buzzer(void);
 
 #endif /* __MNSL_TIMER_H__ */
